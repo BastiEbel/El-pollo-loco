4e2ec8f4ebb64e55a7b0520d83ca36b88b0da289
Sebastian Ebel <sebastianebel@hotmail.de>
2021-08-29 11:30:10 +0200

    walking sound added

--- classes/character.class.js
+++ classes/character.class.js
@@ -10,6 +10,7 @@ class Character extends MovableObject {
     ];
 
     world;
+    walking_sound = new Audio('audio/walking.mp3');
 
 
     constructor() {
@@ -21,14 +22,17 @@ class Character extends MovableObject {
 
     animate() {
         setInterval(() => {
+            this.walking_sound.pause();
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
 
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
 
             this.world.camera_x = -this.x + 100;
