96915322eff3623d992df8be49f8b0404987a144
Sebastian Ebel <sebastianebel@hotmail.de>
2021-08-29 20:24:59 +0200

    hitbox

--- classes/movable.object.class.js
+++ classes/movable.object.class.js
@@ -10,6 +10,7 @@ class MovableObject {
     speedY = 0;
     acceleration = 2.5;
     energy = 100;
+    lastHit = 0;
 
     imageCache = [];
 
@@ -53,15 +54,34 @@ class MovableObject {
         }
     }
 
-    isColliding(move){
+    isColliding(move) {
         return this.x + this.width > move.x &&
-        this.y + this.height > move.y &&
-        this.x < move.x &&
-        this.y < move.y + move.height;
+            this.y + this.height > move.y &&
+            this.x < move.x &&
+            this.y < move.y + move.height;
+    }
+
+    hit() {
+        this.energy -= 5;
+        if (this.energy < 0) {
+            this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
+        }
+    }
+
+    isDead() {
+        return this.energy == 0;
+    }
+
+    isHurt() {
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
     }
 
     playAnimation(images) {
-        let i = this.currentimage % this.IMAGES_WALKING.length;
+        let i = this.currentimage % images.length;
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentimage++;
